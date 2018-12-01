37fc4c527b4d612653e263715637503fb1782646
Guvanch <gnb993@yandex.com>
2018-12-01 16:00:34 +0300

    new: poet page

---
+++ pages/_poetUrl/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <profile-view :data="poet" />
+    <h1 class="title">Goşgylar</h1>
+    <poem-list 
+      :data="poems"
+      :poet="poet" />
+  </div>
+</template>
+
+<script>
+import ProfileView from '@/components/ProfileView'
+import PoemList from '@/components/PoemList'
+
+  export default {
+    components: {
+      'profile-view': ProfileView,
+      'poem-list': PoemList
+    },
+    computed: {
+      poet() {
+        return this.$store.getters['poet/getOne']
+      },
+      poems() {
+        return this.$store.getters['poem/getAll']
+      }
+    },
+    beforeCreate() {
+      const poetUrl = this.$route.params.poetUrl
+      this.$store.dispatch('poet/fetchOne', poetUrl)
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
