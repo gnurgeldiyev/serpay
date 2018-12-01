e5f6ab88a6cd3e6953f6c8fa6b00571543ea6f66
Guvanch <gnb993@yandex.com>
2018-12-01 12:32:46 +0300

    new: poet vuex state controls file

---
+++ store/poet.js
@@ -0,0 +1,78 @@
+export const state = () => ({
+  poet: {
+    url: 'gurbannazar-ezizow',
+    name: 'Gurbannazar Ezizow',
+    avatar: 'https://pbs.twimg.com/profile_images/425363604968591361/7tTT1z33_400x400.jpeg',
+    bio: 'Gurbannazar Ezizow - talantly türkmen şahyry, Türkmenistanyň Magtymguly adyndaky Döwlet baýragynyň eýesi.'
+  },
+  poets: [
+    {
+      name: 'Gurbannazar Ezizow',
+      avatar: 'https://pbs.twimg.com/profile_images/425363604968591361/7tTT1z33_400x400.jpeg',
+      total: 139,
+      url: 'gurbannazar-ezizow'
+    },
+    {
+      name: 'Magtymguly Pyragy',
+      avatar: 'https://pbs.twimg.com/profile_images/1079796065/magtymguly2_400x400.jpg',
+      total: 234,
+      url: 'magtymguly'
+    },
+    {
+      name: 'Berdinazar Hudaýnazarow Serdarogly',
+      avatar: 'http://gollanma.com/wp-content/uploads/2012/02/Berdinazar-Hudaynazarow-new.jpg',
+      total: 127,
+      url: 'berdinazar-hudaynazarow'
+    },
+    {
+      name: 'Gurbannazar Ezizow',
+      avatar: 'https://pbs.twimg.com/profile_images/425363604968591361/7tTT1z33_400x400.jpeg',
+      total: 139,
+      url: 'gurbannazar-ezizow'
+    },
+    {
+      name: 'Magtymguly Pyragy',
+      avatar: 'https://pbs.twimg.com/profile_images/1079796065/magtymguly2_400x400.jpg',
+      total: 234,
+      url: 'magtymguly'
+    },
+    {
+      name: 'Berdinazar Hudaýnazarow Serdarogly',
+      avatar: 'http://gollanma.com/wp-content/uploads/2012/02/Berdinazar-Hudaynazarow-new.jpg',
+      total: 127,
+      url: 'berdinazar-hudaynazarow'
+    }
+  ]
+})
+
+export const getters = {
+  getAll: (state) => {
+    return state.poets
+  },
+  getOne: (state) => {
+    return state.poet
+  }
+}
+
+export const mutations = {
+  setAll: (state, data) => {
+    state.poets = data
+  },
+  setOne: (state, data) => {
+    state.poet = data
+  }
+}
+
+export const actions = {
+  fetchOne({ commit }, poetUrl) {
+    console.log(poetUrl)
+    // return this.$axios.$get(`/posts/${postId}`)
+    // .then((response) => {
+    //   commit('setOne', response.post);
+    //   return true;
+    // })
+    // .catch((err) => {
+    //   return false;
+    // });
+  },
+}
