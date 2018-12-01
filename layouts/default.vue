9abeae0cbb5de64527d3156a8be71e0921b91e64
Guvanch <gnb993@yandex.com>
2018-12-01 12:19:09 +0300

    new: default layout created

---
+++ layouts/default.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <app-header :brand="brand" />
+    <section class="container">
+      <nuxt/>
+    </section>
+    <app-footer />
+  </div>
+</template>
+<script>
+import AppHeader from '@/components/Navigation/AppHeader'
+import AppFooter from '@/components/Navigation/AppFooter'
+
+export default {
+	components: {
+		'app-header': AppHeader,
+		'app-footer': AppFooter
+	},
+	data() {
+		return {
+			brand: {
+				title: 'Serpaý',
+				subtitle: 'Goşgylar Çemeni'
+			}
+		}
+	}
+}
+</script>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Avenir',
+    Roboto, 'Helvetica Neue', Helvetica, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+*:hover {
+	cursor: default;
+}
+@font-face {
+	font-family:'Inria Serif';
+	src:	url('../assets/font/InriaSerif-Italic.eot');
+	src:	url('../assets/font/InriaSerif-Italic.eot?#iefix') format('embedded-opentype'),
+			url('../assets/font/InriaSerif-Italic.woff2') format('woff2'),
+			url('../assets/font/InriaSerif-Italic.woff') format('woff'),
+			url('../assets/font/InriaSerif-Italic.svg') format('svg');
+	font-weight: 400;
+	font-style: italic;
+}
+@font-face {
+	font-family:'Inria Serif';
+	src:	url('../assets/font/InriaSerif-Regular.eot');
+	src:	url('../assets/font/InriaSerif-Regular.eot?#iefix') format('embedded-opentype'),
+			url('../assets/font/InriaSerif-Regular.woff2') format('woff2'),
+			url('../assets/font/InriaSerif-Regular.woff') format('woff'),
+			url('../assets/font/InriaSerif-Regular.svg') format('svg');
+	font-weight: 400;
+	font-style: normal;
+}
+@font-face {
+	font-family:'Inria Serif';
+	src:	url('../assets/font/InriaSerif-Bold.eot');
+	src:	url('../assets/font/InriaSerif-Bold.eot?#iefix') format('embedded-opentype'),
+			url('../assets/font/InriaSerif-Bold.woff2') format('woff2'),
+			url('../assets/font/InriaSerif-Bold.woff') format('woff'),
+			url('../assets/font/InriaSerif-Bold.svg') format('svg');
+	font-weight: 700;
+	font-style: normal;
+}
+@media (max-width: 480px) {
+  .container {
+    min-height: 100vh;
+    width: 100vw;
+    padding: 16px;
+  }
+}
+@media (min-width: 481px) and (max-width: 767px) {
+  .container {
+    min-height: 100vh;
+    width: 100vw;
+    padding: 16px 24px;
+  }
+}
+@media (min-width: 768px) and (max-width: 1087px) {
+  .container {
+    min-height: 100vh;
+    width: 100vw;
+    padding: 32px;
+  }
+}
+@media (min-width: 1088px) {
+  .container {
+    min-height: 100vh;
+    width: 1024px;
+    margin: 0 auto;
+    padding: 32px 0;
+  }
+}
+</style>
