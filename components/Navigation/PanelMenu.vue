d662588ae887d68fe7f56b0f71b920b7921b9876
Guvanch <gnb993@yandex.com>
2018-12-02 14:54:33 +0300

    new: panel menu ui component

---
+++ components/Navigation/PanelMenu.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-menu mode="vertical">
+    <el-menu-item
+      index="1"
+      @click="goTo('/@serpay/poems')">
+      <span>Poems</span>
+    </el-menu-item>
+    <el-menu-item
+      index="2"
+      @click="goTo('/@serpay/poets')">
+      <span>Poets</span>
+    </el-menu-item>
+    <el-menu-item
+      v-if="true"
+      index="3"
+      @click="goTo('/@serpay/editors')">
+      <span>Editors</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+  export default {
+    methods: {
+      goTo(url) {
+        this.$router.push(url);
+      },
+    }
+  }
+</script>
