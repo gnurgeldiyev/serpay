1d7dbd201e59ec831766c8e3097e0051dc1e66db
Guvanch <gnb993@yandex.com>
2018-12-02 14:54:42 +0300

    new: panel header ui component

---
+++ components/Navigation/PanelHeader.vue
@@ -0,0 +1,65 @@
+<template>
+  <el-row :gutter="20">
+    <el-col :span="8">
+      <div>
+        <nuxt-link 
+          to="/@serpay" 
+          class="pheader_logo">
+          Serpaý
+        </nuxt-link>
+        <span>editor</span>
+      </div>
+    </el-col>
+    <el-col :span="16">
+      <ul class="pheader_menu">
+        <li class="pheader_menu_item">
+          <nuxt-link 
+            to="/" 
+            target="_blank"
+            title="Serpaý, Goşgular Çemeni">serpay.penjire.com</nuxt-link>
+        </li>
+      </ul>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+.pheader {
+  padding: 16px 32px;
+  -webkit-box-shadow: 0px 2px 10px 2px rgba(221, 221, 221, 0.5);
+  -moz-box-shadow: 0px 2px 10px 2px rgba(221, 221, 221, 0.5);
+  box-shadow: 0px 2px 10px 2px rgba(221, 221, 221, 0.5);
+}
+.pheader_logo {
+  color: #000001;
+  text-decoration: none;
+  font-size: 2rem;
+  line-height: 1;
+  font-weight: 700;
+  font-family: 'Inria Serif', Georgia, serif;
+}
+.pheader_menu {
+  float: right;
+  list-style: none;
+  margin-top: 6px;
+}
+.pheader_menu_item {
+  display: inline;
+  margin: 0 20px;
+}
+.pheader_menu_item a {
+  color: #656565;
+  text-decoration: none;
+  font-family: inherit;
+  font-weight: 500;
+}
+.pheader_menu_item a:hover {
+  cursor: pointer;
+}
+</style>
