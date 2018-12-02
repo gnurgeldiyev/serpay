69baa397adaa55b6e0d38b063b4c5e9f7c885a31
Guvanch <gnb993@yandex.com>
2018-12-02 13:32:07 +0300

    new: poem page

---
+++ pages/_poetUrl/_poemUrl/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <poem-view :data="poem" />
+  </div>
+</template>
+
+<script>
+import PoemView from '@/components/PoemView'
+
+  export default {
+    components: {
+      'poem-view': PoemView
+    },
+    computed: {
+      poem() {
+        return this.$store.getters['poem/getOne']
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
