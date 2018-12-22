64b43a2ca66a6999daeca773ba49f28a74219d6f
Guvanch <gnb993@yandex.com>
2018-12-22 14:03:43 +0300

    change: poet naming conventions

--- store/poet.js
+++ store/poet.js
@@ -1,29 +1,11 @@
 export const state = () => ({
-  poet: {
+  one: {
     url: 'gurbannazar-ezizow',
     name: 'Gurbannazar Ezizow',
     avatar: 'https://pbs.twimg.com/profile_images/425363604968591361/7tTT1z33_400x400.jpeg',
     bio: 'Gurbannazar Ezizow - talantly türkmen şahyry, Türkmenistanyň Magtymguly adyndaky Döwlet baýragynyň eýesi.'
   },
-  poets: [
-    {
-      name: 'Gurbannazar Ezizow',
-      avatar: 'https://pbs.twimg.com/profile_images/425363604968591361/7tTT1z33_400x400.jpeg',
-      total: 139,
-      url: 'gurbannazar-ezizow'
-    },
-    {
-      name: 'Magtymguly Pyragy',
-      avatar: 'https://pbs.twimg.com/profile_images/1079796065/magtymguly2_400x400.jpg',
-      total: 234,
-      url: 'magtymguly'
-    },
-    {
-      name: 'Berdinazar Hudaýnazarow',
-      avatar: 'http://gollanma.com/wp-content/uploads/2012/02/Berdinazar-Hudaynazarow-new.jpg',
-      total: 127,
-      url: 'berdinazar-hudaynazarow'
-    },
+  all: [
     {
       name: 'Gurbannazar Ezizow',
       avatar: 'https://pbs.twimg.com/profile_images/425363604968591361/7tTT1z33_400x400.jpeg',
@@ -46,20 +28,20 @@ export const state = () => ({
 })
 
 export const getters = {
-  getAll: (state) => {
-    return state.poets
+  all: (state) => {
+    return state.all
   },
-  getOne: (state) => {
-    return state.poet
+  one: (state) => {
+    return state.one
   }
 }
 
 export const mutations = {
-  setAll: (state, data) => {
-    state.poets = data
+  all: (state, data) => {
+    state.all = data
   },
-  setOne: (state, data) => {
-    state.poet = data
+  one: (state, data) => {
+    state.one = data
   }
 }
 
