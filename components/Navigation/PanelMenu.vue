d04e76be1e2d3425b7de6e1af0d089ad7abd1eaf
Guvanch <gnb993@yandex.com>
2018-12-22 15:11:57 +0300

    update: router improves

--- components/Navigation/PanelMenu.vue
+++ components/Navigation/PanelMenu.vue
@@ -1,19 +1,18 @@
 <template>
-  <el-menu mode="vertical">
+  <el-menu 
+    mode="vertical"
+    router>
     <el-menu-item
-      index="1"
-      @click="goTo('/@serpay/poems')">
+      index="/@serpay/poems">
       <span>Poems</span>
     </el-menu-item>
     <el-menu-item
-      index="2"
-      @click="goTo('/@serpay/poets')">
+      index="/@serpay/poets">
       <span>Poets</span>
     </el-menu-item>
     <el-menu-item
       v-if="true"
-      index="3"
-      @click="goTo('/@serpay/editors')">
+      index="/@serpay/editors">
       <span>Editors</span>
     </el-menu-item>
   </el-menu>
@@ -21,10 +20,6 @@
 
 <script>
   export default {
-    methods: {
-      goTo(url) {
-        this.$router.push(url);
-      },
-    }
+    
   }
 </script>
