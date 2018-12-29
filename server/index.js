289597e148e1dc3a262bbf2be54dbfa620013f3a
Guvanch <gnb993@yandex.com>
2018-12-29 13:10:55 +0300

    update: connect to mongodb with mongoose

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
-
 const express = require('express')
 const consola = require('consola')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
 const { node_env, host, port, mongodb_uri } = require('../config')
 
 app.set('port', port)
+
+// Setup MongoDB with mongoose
+mongoose.Promise = global.Promise
+mongoose.connect(mongodb_uri, { useNewUrlParser: true, autoIndex: false })
+mongoose.connection.on('error', (err) => {
+  console.log(`MongoDB connection is failed → ${err.message}`)
+})
 
 // request body parser middleware
 app.use(bodyParser.json())
