3040755d9615510850b66ca7453c24eb6417013f
Guvanch <gnb993@yandex.com>
2018-12-29 13:10:31 +0300

    update: app use body parser middleware

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 
 const express = require('express')
 const consola = require('consola')
+const bodyParser = require('body-parser')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
 const { node_env, host, port, mongodb_uri } = require('../config')
 
 app.set('port', port)
+
+// request body parser middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // Import and Set Nuxt.js options
 let config = require('../nuxt.config.js')
