5516582698810cb2598489453f3037c83792efde
Guvanch <gnb993@yandex.com>
2019-01-01 17:50:53 +0300

    update: import poet routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 const editorRoutes = require('./editor')
+const poetRoutes = require('./poet')
 
 // Import Routes
 module.exports = (app) => {
   app.use('/api/editors', editorRoutes)
+  app.use('/api/poets', poetRoutes)
 }
