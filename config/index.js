da229d9443b765eea43c1d07d91c1a3621189eaa
Guvanch <gnb993@yandex.com>
2019-01-01 18:47:28 +0300

    update: new config variable

--- config/index.js
+++ config/index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const node_env = process.env.NODE_ENV
 const host = process.env.HOST
 const port = process.env.PORT
+const base_url = process.env.BASE_URL
 const mongodb_uri = process.env.MONGODB_URI
 const password_salt = process.env.PASSWORD_SALT
 
@@ -10,6 +11,7 @@ module.exports = {
   node_env,
   host,
   port,
+  base_url,
   mongodb_uri,
   password_salt
 }
