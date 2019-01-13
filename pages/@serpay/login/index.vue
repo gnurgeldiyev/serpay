f3e75c23a836acd568fb13d43312ddaac785bcb3
Guvanch <gnb993@yandex.com>
2019-01-13 22:23:40 +0300

    new: login page

---
+++ pages/@serpay/login/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <login-form />
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/Panel/LoginForm'
+
+  export default {
+    components: {
+      'login-form': LoginForm
+    } 
+  }
+</script>
+
+<style scoped>
+.container {
+  width: 360px;
+  height: auto;
+}
+</style>
