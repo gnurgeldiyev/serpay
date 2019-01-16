f7f8e4ec928ea6cbdea3486871304cf69400b36b
Guvanch <gnb993@yandex.com>
2019-01-16 22:28:31 +0300

    update: logout functionality

--- components/Navigation/PanelHeader.vue
+++ components/Navigation/PanelHeader.vue
@@ -7,7 +7,10 @@
           class="pheader_logo">
           Serpaý
         </nuxt-link>
-        <span>editor</span>
+        <span 
+          v-if="editor">
+          {{ editor.role }}
+        </span>
       </div>
     </el-col>
     <el-col :span="16">
@@ -16,7 +19,23 @@
           <nuxt-link 
             to="/" 
             target="_blank"
-            title="Serpaý, Goşgular Çemeni">serpay.penjire.com</nuxt-link>
+            title="Serpaý, Goşgular Çemeni">
+            serpay.penjire.com
+          </nuxt-link>
+        </li>
+        <li class="pheader_menu_item">
+          <el-dropdown trigger="click">
+            <span class="el-icon-more icon" />
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>
+                <el-button 
+                  type="text"
+                  @click="handleLogout(editor.id)">
+                  Logout
+                </el-button>
+              </el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
         </li>
       </ul>
     </el-col>
@@ -25,7 +44,28 @@
 
 <script>
   export default {
-
+    computed: {
+      editor() {
+        return this.$store.getters['editor/getOne']
+      }
+    },
+    methods: {
+      async handleLogout(data) {
+        const result = await this.$store.dispatch('editor/logout', data)
+        if (!result.status) {
+          this.$message({
+            message: 'An error occurred.',
+            type: 'error'
+          })
+        } else {
+          this.$router.push('/')
+          this.$message({
+            message: 'Logged out.',
+            type: 'success'
+          })
+        }
+      }
+    }
   }
 </script>
 
@@ -61,5 +101,8 @@
 }
 .pheader_menu_item a:hover {
   cursor: pointer;
+}
+.pheader_menu_item .icon {
+  font-size: 1.5em;
 }
 </style>
