a6332ffbf0594f6af3daa293bb0aa944b0a3b01c
Guvanch <gnb993@yandex.com>
2019-01-19 12:34:04 +0300

    update: configurations

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
-const pkg = require('./package')
 const webpack = require('webpack')
-const { base_url } = require('./config')
+const { base_url, node_env } = require('./config')
+const isDebugged = node_env !== 'production' ? true : false
 
 module.exports = {
   mode: 'universal',
@@ -8,11 +8,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Serpaý – Goşgular Çemeni',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: 'Türkmen edebiýatyndan goşgular çemeni' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -54,7 +54,7 @@ module.exports = {
   axios: {
     baseURL: base_url,
     credentials: true,
-    debug: true,
+    debug: isDebugged,
     retry: {
       retries: 3
     },
@@ -64,30 +64,9 @@ module.exports = {
   ** Build configuration
   */
   build: {
-    /** 
-     * html.minify configs
-    */
-    collapseBooleanAttributes: true,
-    collapseWhitespace: false,
-    decodeEntities: true,
-    minifyCSS: true,
-    minifyJS: true,
-    processConditionalComments: true,
-    removeAttributeQuotes: false,
-    removeComments: false,
-    removeEmptyAttributes: true,
-    removeOptionalTags: false,
-    removeRedundantAttributes: true,
-    removeScriptTypeAttributes: false,
-    removeStyleLinkTypeAttributes: false,
-    removeTagWhitespace: false,
-    sortClassName: false,
-    trimCustomFragments: true,
-    useShortDoctype: true,
     /*
     ** You can extend webpack config here
     */
-    minimize: true,
     plugins: [
       new webpack.ProvidePlugin({
         'window.Quill': 'quill'
