46934cc5dab674d6e60cef30c36c7a8a66a09b99
Guvanch <gnb993@yandex.com>
2019-02-06 12:10:16 +0300

    new: server api caching middleware

---
+++ common/cache.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import apicache from 'apicache'
+
+const app = express()
+
+// https://github.com/kwhitley/apicache
+app.use(apicache.middleware('10 minutes'))
+
+// apicache.options({ debug: true })
+
+export default {
+  path: '/api/',
+  handler: app
+}
